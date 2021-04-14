2cff4e4f273931b70b715caac44e91107254c996
YuryVA <ghost_5990@mail.ru>
2021-04-14 21:46:02 +0300

    fifth commit homework1. test_task3 change quotes

--- tests/homework1/test_task3.py
+++ tests/homework1/test_task3.py
@@ -2,4 +2,4 @@ from homework1.task3 import find_maximum_and_minimum
 
 
 def test_find_maximum_and_minimum():
-    assert find_maximum_and_minimum('homework1/task3_test.txt') == (3, 78)
+    assert find_maximum_and_minimum("homework1/task3_test.txt") == (3, 78)

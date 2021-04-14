94f132cb7ba72f006c5359e680eb4198f41e1c48
YuryVA <ghost_5990@mail.ru>
2021-04-14 20:41:04 +0300

    third commit homework1. Change "== bool statement" on "is bool"

--- tests/homework1/test_task2.py
+++ tests/homework1/test_task2.py
@@ -1,7 +1,7 @@
 from homework1.task2 import check_fibonacci
 
 if __name__ == '__main__':
-    assert check_fibonacci([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]) == True
-    assert check_fibonacci([0, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]) == False
-    assert check_fibonacci([-55, 34, -21, 13, -8, 5, -3, 2, -1]) == True
-    assert check_fibonacci([1, 1, 2, 3, 4, 8, 13, 21, 34, 55]) == False
+    assert check_fibonacci([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]) is True
+    assert check_fibonacci([0, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]) is False
+    assert check_fibonacci([-55, 34, -21, 13, -8, 5, -3, 2, -1]) is True
+    assert check_fibonacci([1, 1, 2, 3, 4, 8, 13, 21, 34, 55]) is False
